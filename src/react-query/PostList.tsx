586960e8aafddcae2a2e4c32aa963baac2620195
JoshThanki <joshthanki@gmail.com>
2023-08-25 13:30:44 +0100

    added filtering by users

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,34 +1,34 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
-interface Post {
-  id: number;
-  title: string;
-  body: string;
-  userId: number;
-}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import usePosts from "../routing/hooks/usePosts";
 
 const PostList = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [error, setError] = useState('');
+  const [userId, setUserId] = useState<number>();
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => setPosts(res.data))
-      .catch((error) => setError(error));
-  }, []);
+  const { data, error, isLoading } = usePosts(userId);
 
-  if (error) return <p>{error}</p>;
+  if (error) return <p>{error.message}</p>;
 
+  const userList = ["", 1, 2, 3];
   return (
-    <ul className="list-group">
-      {posts.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select
+        className="form-select mb-3"
+        value={userId}
+        onChange={(event) => setUserId(parseInt(event.target.value))}
+      >
+        {userList.map((index) => (
+          <option value={index}> {index ? "User " + index : "All"} </option>
+        ))}
+      </select>
+      <ul className="list-group">
+        {data?.map((post) => (
+          <li key={post.id} className="list-group-item">
+            {post.title}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
---
+++ src/routing/hooks/usePosts.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+    userId: number;
+  }
+
+const usePosts = (userId: number | undefined) => {
+    const fetchPosts = () => axios
+    .get('https://jsonplaceholder.typicode.com/posts', {params: {userId}})
+    .then((res) => res.data);
+
+    return useQuery<Post[], Error>({
+        queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+        queryFn: fetchPosts,
+        staleTime: 1 * 60 * 1000
+    });
+}
+
+export default usePosts;
